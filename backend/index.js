16ed5f43874f321d265d78b3d641fcac50ee2e20
ian firmino <ian.firmino@hotmail.com>
2024-09-15 12:51:56 -0300

    feat: create folder structure

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../frontend'));
 
 app.use(express.json());
 app.use(express.static('frontend'));
 
-app.get('/home', (req, res) => {
-    res.send('olá');
+app.get('/new', (req, res) => {
+    res.render('new')
+});
+
+app.get('/menu', (req, res) => {
+    res.render('menu')
 });
 
 app.listen(port, () => {
-    console.log(`api rodando em http://localhost:${port}`);
+    console.log(`rodando em http://localhost:${port}`);
 });
